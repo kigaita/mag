b602997aa12f26d1f20fad7215ab75ec1ce44e02
pborawski <pborawski@xcode.pl>
2016-04-18 20:14:05 +0200

    Widok dodawania druzyny

---
+++ src/UwbItContest.Web/Features/Team/UiController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+namespace UwbItContest.Web.Features.Team
+{
+    public class UiController : Controller
+    {
+        public ActionResult Add() => View();
+
+        [HttpPost]
+        public ActionResult Add(Add.Command command)
+        {
+            return RedirectToAction("Index", "User");
+        }
+    }
+}
